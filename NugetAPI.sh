bdbf280830ad0497035d922de76505b84d3602e4
Divyanshu Agarwal <104520117+Divyanshu8755@users.noreply.github.com>
2025-12-14 21:31:31 +0530

    Update NugetAPI.sh

--- NugetAPI.sh
+++ NugetAPI.sh
@@ -6,16 +6,18 @@ PackageName="newtonsoft.json"
 PackageVersion="13.0.3"
 CacheDir=".nuget-cache"
 
-PackageFile="${CacheDir}/${PackageName}.${PackageVersion}.nupkg"
-NugetURL="https://api.nuget.org/v3-flatcontainer/${PackageName}/${PackageVersion}/${PackageName}.${PackageVersion}.nupkg"
+LowerPackageName=$(echo "$PackageName" | tr '[:upper:]' '[:lower:]')
+
+PackageFile="${CacheDir}/${LowerPackageName}.${PackageVersion}.nupkg"
+NugetURL="https://api.nuget.org/v3-flatcontainer/${LowerPackageName}/${PackageVersion}/${LowerPackageName}.${PackageVersion}.nupkg"
 
 mkdir -p "$CacheDir"
 
 if [ ! -f "$PackageFile" ]; then
-	echo "donwloading ${PackageName} ${PackageVersion} via Nuger API...."
-	echo "URL: $NugetURL"
-	curl -L "$NugetURL" -o "$PackageFile"
+        echo "donwloading ${PackageName} ${PackageVersion} via Nuger API...."
+        echo "URL: $NugetURL"
+        curl -L "$NugetURL" -o "$PackageFile"
 
-else 
-	echo "Package already exists. skipping download."
+else
+        echo "Package already exists. skipping download."
 fi
